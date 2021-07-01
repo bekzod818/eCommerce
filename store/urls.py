0a0132d0200fef8eb26fa2ccbcf36b517a32b1ef
bekzod818 <bekzod030900@gmail.com>
2021-07-01 19:21:22 +0500

    ecom

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('card/', views.card, name='card'),
-    path('checkout/', views.checkout, name='checkout')
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order')
 ]
